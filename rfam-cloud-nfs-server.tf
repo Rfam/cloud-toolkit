b27d22fa07582105862a3e02c52a8b93fa0a4ace
ikalvari <ikalvari@ebi.ac.uk>
2019-05-16 17:57:51 +0100

    Code to create an instance with a volume and floating ip

---
+++ rfam-cloud-nfs-server.tf
@@ -0,0 +1,94 @@
+# Terraform configuration to launch an NFS server in an openstack tenancy
+
+# terraform variable declaration
+
+variable "os_image" {
+	default = ""
+}
+
+variable "flavor_name" {
+	default = ""
+}
+
+variable "flavor_id" {
+	default = ""
+}
+
+variable "keypair" {
+	default = ""
+}
+
+variable "security_group" {
+	default = ""
+}
+
+variable "network_name" {
+	default = ""
+}
+
+variable "floating_ip_pool" {
+	default = ""
+}
+
+variable "ssh_user"{
+	default = "ubuntu"
+}
+
+#resource "openstack_images_image_v2" "ubuntu_xenial" {
+#  name   = "ubuntu_xenial"
+#  image_source_url = "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img"
+#  container_format = "bare"
+#  disk_format = "qcow2"
+
+#  properties = {
+#    key = "value"
+#  }
+#}
+
+
+## create a new volume
+resource "openstack_blockstorage_volume_v2" "nfs_vol" {
+  name = "test_nfs_vol"
+  size = 10
+}
+
+###############------------INSTANCES-----------###############
+
+
+# create a new openstack instance
+resource "openstack_compute_instance_v2" "nfs_server" {
+  name              = "test_nfs_server"
+  image_name        = "${var.os_image}"
+  flavor_name       = "${var.flavor_name}" 
+  key_pair          = "${var.keypair}"
+  security_groups   = ["${var.security_group}"]
+
+  # asign instance to a network
+  #connection {
+  #private_key = "./ssh_key"
+  #}
+
+  # the network to be attached to the instance
+  network {
+    name = "${var.network_name}"
+  }
+}
+
+# attach volume to instance
+resource "openstack_compute_volume_attach_v2" "attached" {
+  instance_id = "${openstack_compute_instance_v2.nfs_server.id}"
+  volume_id = "${openstack_blockstorage_volume_v2.nfs_vol.id}"
+}
+
+# fetch floating ip from floating ip pool
+resource "openstack_networking_floatingip_v2" "nfs_server_floating_ip" {
+  pool = "${var.floating_ip_pool}"
+}
+
+# associate floating IP to instance
+resource "openstack_compute_floatingip_associate_v2" "nfs_server_floating_ip" {
+  floating_ip = "${openstack_networking_floatingip_v2.nfs_server_floating_ip.address}"
+  instance_id = "${openstack_compute_instance_v2.nfs_server.id}"
+}
+
+
